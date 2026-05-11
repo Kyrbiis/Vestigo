bdee66c9a1ca249cd285563cf5ff93bad57960ce
Jojo Hyman <jojobhyman@gmail.com>
2026-05-10 20:38:31 -0400

    Initial Commit

---
+++ Vestigo/VestigoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VestigoApp.swift
+//  Vestigo
+//
+//  Created by Jojo Hyman on 5/10/26.
+//
+
+import SwiftUI
+
+@main
+struct VestigoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
